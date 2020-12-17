697d146a73ef85d969edc91509b5aabdb7ab8635
Abidi Amal <abidiamal77@gmail.com>
2020-12-17 12:12:05 +0100

    completing factorys

---
+++ database/factories/MealFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Meal;
+use Faker\Generator as Faker;
+
+$factory->define(Meal::class, function (Faker $faker) {
+    return [
+        'name' => $faker->foodName,
+        'photo' => $faker->imageUrl,
+        'description' => $faker->paragraph, 
+        'quantity_in_stock' => $faker->randomDigit ,
+        'buy_price' => $faker->randomFloat(3,2,150) ,
+        'sale_price' => $faker->randomFloat(3,2,150) ,
+        'created_at' => now(),
+        'updated_at' => now()
+    ];
+});
